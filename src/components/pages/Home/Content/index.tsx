134dce3144a01e23c0251d30d8609ed63e5c193f
migz <migz.codes@gmail.com>
2025-09-14 12:40:12 -0300

    feat: camera and helpers

--- src/components/pages/Home/Content/index.tsx
+++ src/components/pages/Home/Content/index.tsx
@@ -1,22 +1,25 @@
 'use client'
 
-import { Canvas, useFrame } from '@react-three/fiber'
+import { GizmoHelper, GizmoViewcube, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import { useRef } from 'react'
 import type * as THREE from 'three'
 
 export const Cube = () => {
   const cube = useRef<THREE.Mesh>(null)
 
-  useFrame(() => {
-    if (!cube.current) return
+  // useFrame(() => {
+  //   if (!cube.current) return
 
-    cube.current.rotation.x += 0.05
-    cube.current.rotation.y += 0.05
-    cube.current.rotation.z += 0.05
-  })
+  //   cube.current.rotation.x += 0.05
+  //   cube.current.rotation.y += 0.05
+  //   cube.current.rotation.z += 0.05
+  // })
 
   return (
     <mesh ref={cube}>
+      <axesHelper args={[10]} />
+
       <boxGeometry args={[2, 2, 2]} />
       <meshPhongMaterial color='#666cff' />
     </mesh>
@@ -26,8 +29,13 @@ export const Cube = () => {
 export const Content = () => (
   <main className='w-screen h-screen flex flex-col'>
     <Canvas className='bg-primary-500 w-[50%] h-full' camera={{ position: [2, 2, 2] }}>
-      <Cube />
+      <gridHelper args={[10, 10, '#f00', '#fff']} />
+      <GizmoHelper alignment='bottom-right' margin={[80, 80]} scale={100}>
+        <GizmoViewcube />
+      </GizmoHelper>
 
+      <OrbitControls />
+      <Cube />
       <directionalLight position={[2, 5, 1]} />
     </Canvas>
   </main>
