d31b3e5c73d004f062d754322d1264d3177bf682
migz <migz.codes@gmail.com>
2025-09-14 12:19:01 -0300

    feat: first animation

--- src/components/pages/Home/Content/index.tsx
+++ src/components/pages/Home/Content/index.tsx
@@ -1,23 +1,34 @@
 'use client'
 
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import type * as THREE from 'three'
+
+export const Cube = () => {
+  const cube = useRef<THREE.Mesh>(null)
+
+  useFrame(() => {
+    if (!cube.current) return
+
+    cube.current.rotation.x += 0.05
+    cube.current.rotation.y += 0.05
+    cube.current.rotation.z += 0.05
+  })
+
+  return (
+    <mesh ref={cube}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshPhongMaterial color='#666cff' />
+    </mesh>
+  )
+}
 
 export const Content = () => (
   <main className='w-screen h-screen flex flex-col'>
     <Canvas className='bg-primary-500 w-[50%] h-full' camera={{ position: [2, 2, 2] }}>
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshPhongMaterial color='#666cff' />
-        <directionalLight position={[2, 5, 1]} />
-      </mesh>
-    </Canvas>
+      <Cube />
 
-    <Canvas className='bg-primary-500 w-full h-full' camera={{ position: [2, 2, 2] }}>
-      <mesh>
-        <sphereGeometry args={[1, 80, 80]} />
-        <meshPhongMaterial color='#666cff' />
-        <directionalLight position={[2, 5, 1]} />
-      </mesh>
+      <directionalLight position={[2, 5, 1]} />
     </Canvas>
   </main>
 )
